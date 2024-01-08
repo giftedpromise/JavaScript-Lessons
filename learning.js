aa1412852b63256b00f5eaf9e57049e952a27e44
giftedpromise <giftedpromise1992@gmail.com>
2024-01-08 03:29:14 +0100

    third day

--- learning.js
+++ learning.js
@@ -191,3 +191,124 @@ b *= 3;
 c *= 10;
 
 console.log(b);
+
+//Compound Assignment with augmented division
+var a = 48;
+var b = 108;
+var c = 33;
+
+a = a / 12;
+b = b / 4;
+c = c / 33;
+
+a /= 12;
+b /= 4;
+c /= 33;
+
+//Escaping literal quotes in strings
+var mySentence = 'I am a "double quoted" string inside "double quotes"';
+
+/* 
+/n - new line
+/t - tab
+/f - form feed
+/b - backspace
+
+*/
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(myStr);
+
+//Concactenating Strings with plus Operator
+
+var ourStr = "I come first" + "I come second.";
+var myStr = "This is the start." + "This is the end";
+
+console.log(myStr);
+
+//Concatenating strings with plus equals operator
+
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+console.log(myStr);
+
+//Constructing strings with variables
+var ourNames = "Worldxcode";
+var ourStr = "Hello, our name is " + ourNames + ", how are you?";
+
+console.log(ourStr);
+
+var myName;
+var myStr;
+
+myName = "Promise";
+myStr = "My name is " + myName + ". I am beautiful";
+console.log(myStr);
+
+//Appendind variables to strings
+var anAdjective = "awesome";
+var ourStr = "Promise is ";
+
+ourStr += anAdjective;
+console.log(ourStr);
+
+var someAdjective = "worthwhile";
+var myStr = "learning to code ";
+myStr += someAdjective;
+
+console.log(myStr);
+
+//find length of string
+var firstNameLength = 0;
+var firstName = "Promise";
+
+firstNameLength = firstName.length;
+console.log(firstNameLength);
+
+//Setup
+
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+lastNameLength = lastName.length;
+console.log(lastNameLength);
+
+//Bracket Notation to find first character in string
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+console.log(firstLetterOfFirstName);
+
+//setup
+var firstLetterOfLastName = "";
+var lastName = "Nwankwo";
+
+firstLetterOfLastName = lastName[0];
+console.log(firstLetterOfLastName);
+
+//string immutability: strings are immutable meaning that they can't be altered once they are created.
+var myStr = "Jello world";
+myStr = "Hello world";
+
+//Bracket Notation to find Nth Character in String
+var firstLetterOfLastName = "";
+var lastName = "Nwankwo";
+
+firstLetterOfLastName = lastName[3];
+console.log(firstLetterOfLastName);
+
+//Bracket Notation to find last Character in String
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+var lastName = "Lovelace";
+var lastLetterOfLastName = lastName[lastName.length - 1];
+console.log(lastLetterOfLastName);
+
+var lastLetterOfLastName = lastName[lastName.length - 3];
+console.log(lastLetterOfLastName);
