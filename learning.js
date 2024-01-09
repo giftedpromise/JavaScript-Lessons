55f6bab6820ff0561930373a869e3ce47a526cd0
giftedpromise <giftedpromise1992@gmail.com>
2024-01-09 06:21:05 +0100

    5th part

--- learning.js
+++ learning.js
@@ -539,3 +539,51 @@ function minusSeven(num) {
 }
 
 console.log(minusSeven(10));
+
+//Example 2
+
+function timesFive(num) {
+  return num * 5;
+}
+
+console.log(timesFive(5));
+
+//Understanding Undefined Value Returned from a Function
+var sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+function addFive() {
+  sum = sum + 5;
+}
+
+//Assignment with a Returned Value
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+processed = processArg(7);
+
+//Stand in line
+
+function nextInLine(arr, item) {
+  arr.push(item);
+  return arr.shift();
+}
+
+var testArr = [1, 2, 3, 4, 5];
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
