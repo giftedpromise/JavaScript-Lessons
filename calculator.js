843e6d01377884305515996df47c97e4207945f6
giftedpromise <giftedpromise1992@gmail.com>
2024-01-22 01:30:57 +0100

    scrimba3

--- calculator.js
+++ calculator.js
@@ -56,3 +56,84 @@ function divisionOperation(a, b) {
   let answer = a / b;
   return answer;
 }
+
+/* const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let arrayOfOperations = ["+", "-", "*", "/"];
+let userInput1 = "";
+let userInput2 = "";
+let userOperation = "";
+
+rl.question("Enter your first value: ", function (string) {
+  userInput1 = parseFloat(string);
+  getUserInput2();
+});
+
+function getUserInput2() {
+  rl.question("Enter your second value: ", function (string) {
+    userInput2 = parseFloat(string);
+    getUserOperation();
+  });
+}
+
+function getUserOperation() {
+  rl.question("Enter your operation (+, -, *, /): ", function (string) {
+    userOperation = string;
+
+    if (!arrayOfOperations.includes(userOperation)) {
+      console.log("Invalid operation. Please enter +, -, *, or /.");
+      rl.close();
+      return;
+    }
+
+    performOperation();
+  });
+}
+
+function performOperation() {
+  switch (userOperation) {
+    case arrayOfOperations[0]:
+      console.log(additionOperation(userInput1, userInput2));
+      break;
+    case arrayOfOperations[1]:
+      console.log(subtractionOperation(userInput1, userInput2));
+      break;
+    case arrayOfOperations[2]:
+      console.log(multiplicationOperation(userInput1, userInput2));
+      break;
+    case arrayOfOperations[3]:
+      if (userInput2 === 0) {
+        console.log("Cannot divide by zero.");
+      } else {
+        console.log(divisionOperation(userInput1, userInput2));
+      }
+      break;
+    default:
+      console.log("Invalid operation.");
+  }
+
+  rl.close();
+}
+
+function additionOperation(a, b) {
+  return a + b;
+}
+
+function subtractionOperation(a, b) {
+  return a - b;
+}
+
+function multiplicationOperation(a, b) {
+  return a * b;
+}
+
+function divisionOperation(a, b) {
+  return a / b;
+}
+
+*/
--- scrimba.js
+++ scrimba.js
@@ -22,3 +22,36 @@ console.log(bonusPoints);
 
 bonusPoints = bonusPoints + 45;
 console.log(bonusPoints);
+
+function myFunction() {
+  console.log(42);
+}
+
+myFunction();
+
+let lap1 = 34;
+let lap2 = 33;
+let lap3 = 36;
+
+// Create a function that logs out the sum of all the lap times
+function logLapTime() {
+  let totalTime = lap1 + lap2 + lap3;
+  console.log(totalTime);
+}
+
+logLapTime();
+
+let lapsCompleted = 0;
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+function incrementLap() {
+  lapsCompleted = lapsCompleted + 1;
+}
+
+incrementLap();
+incrementLap();
+incrementLap();
+
+console.log(lapsCompleted);
