11dff590f2b1e17041bfc2237ef0608a9c8bf804
giftedpromise <giftedpromise1992@gmail.com>
2024-01-22 03:22:26 +0100

    scrimba5

--- scrimba.js
+++ scrimba.js
@@ -75,3 +75,14 @@ incrementLap();
 incrementLap();
 
 console.log(lapsCompleted);
+
+// Create two variables, name and greeting. The name variable should store your name,
+// and the greeting should store e.g. "Hi, my name is "
+let name = "Promise";
+let greeting = "Hi, my name is ";
+// Create a third variable, myGreeting, that contatenates the two strings
+let myGreeting = greeting + name;
+
+// Log myGreeting to the console
+
+console.log(myGreeting);
