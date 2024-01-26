1b8a889f193c4436ea51fcdaafc3021f8521161f
giftedpromise <giftedpromise1992@gmail.com>
2024-01-25 19:26:42 -0500

    blackjack

--- practicescrimba.js
+++ practicescrimba.js
@@ -57,3 +57,40 @@ function purchase() {
   console.log("button clicked");
   errorParagrapgh.innerText = "Something went wrong, please try again";
 }
+
+let num1 = 8;
+let num2 = 2;
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+
+// Create four functions: add(), subtract(), divide(), multiply()
+
+let sumEl = document.getElementById("sum-el");
+
+function add() {
+  let sum = num1 + num2;
+
+  sumEl.innerText += sum;
+}
+
+function subtract() {
+  let minus = num1 - num2;
+
+  sumEl.innerText += minus;
+}
+
+function divide() {
+  let division = num1 / num2;
+  sumEl.innerText += division;
+}
+
+function multiply() {
+  let multi = num1 * num2;
+  sumEl.innerText += multi;
+}
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
