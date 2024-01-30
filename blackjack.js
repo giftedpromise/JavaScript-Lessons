dbd13657d6ccb48012bdab8281855679f61c60d0
giftedpromise <giftedpromise1992@gmail.com>
2024-01-30 02:32:40 +0100

    blackjack2

--- blackjack.js
+++ blackjack.js
@@ -304,6 +304,83 @@ for (let i = 0; i < largeCountries.length; i++) {
     console.log("- " + largeCountries[i])
 }
 
+let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+
+// You need to help me fixup the largeCountries array so that 
+// China and Pakistan are added back into their respective places
+
+// Use push() & pop() and their counterparts unshift() & shift()
+// Google how to use unshift() and shift()
+largeCountries.pop();
+console.log(largeCountries)
+largeCountries.push("Pakistan");
+console.log(largeCountries)
+largeCountries.shift();
+console.log(largeCountries)
+largeCountries.unshift("China");
+console.log(largeCountries)
+
+let dayOfMonth = 13
+let weekday = "Friday"
+
+// If it is Friday the 13th, log out this spooky face: 😱
+// Use the logical "AND operator" -> &&
+
+
+if ( dayOfMonth === 13 && weekday === "Friday") {
+    console.log("If it is Friday the 13th, log out this spooky face: 😱")
+}
+
+
+let hands = ["rock", "paper", "scissor"]
+
+// Create a function that returns a random item from the array
+
+function getHands() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    return hands[randomNumber];
+}
+
+console.log( getHand() );
+
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {
+    // Challenge:
+    // When the user clicks on the "Pick Fighters" button, pick two random 
+    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    let randomEmoji = Math.floor(Math.random() * fighters.length);
+   
+     let randomEmojiTwo = Math.floor(Math.random() * fighters.length);
+    
+    stageEl.textContent = fighters[randomEmoji] + " vs " + fighters[randomEmojiTwo]
+})
+
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+
+// Create a function that puts the apples onto the appleShelf
+// and the oranges onto the orangeShelf. Use a for loop,
+// a conditional statement, and the textContent property.
+function sortFruit() {
+for (i = 0; i < fruit.length; i++) {
+    if  (fruit[i] === "🍎") {
+        appleShelf.textContent = fruit[i]
+    } else if (fruit[i] === "🍊") {
+        orangeShelf.textContent = fruit[i]
+    }
+}
+
+}
+
+sortFruit();
+
+
+
 */
 
 let cards = [];
