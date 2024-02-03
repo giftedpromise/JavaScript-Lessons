629f25ba5f7d51191fe15b884e392c1ea686360c
giftedpromise <giftedpromise1992@gmail.com>
2024-02-03 22:37:11 +0100

    1st of February

--- chromeextension.js
+++ chromeextension.js
@@ -67,6 +67,15 @@ function buy() {
   li.textContent = myLeads[i];
   ulEl.append(li);
 }
+
+// template strings/literals
+
+const recipient = "James"
+
+// Refactor the email string to use template strings
+const email = `Hey   ${recipient} ! How is it going? Cheers Per`
+
+console.log(email)
 */
 
 let myLeads = [];
