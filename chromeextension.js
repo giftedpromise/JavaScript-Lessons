b59fe726b85387f436b8220d276d7f360c2eeb05
giftedpromise <giftedpromise1992@gmail.com>
2024-02-05 09:04:15 +0100

    1st of February

--- chromeextension.js
+++ chromeextension.js
@@ -98,6 +98,35 @@ if (credits) {
 // null
 // undefined
 // NaN
+
+const welcomeEl = document.getElementById("welcome-el")
+
+// Give the function a parameter, greeting, that replaces "Welcome back"
+function greetUser(greeting) {
+    welcomeEl.textContent = greeting + ", Per Harald Borgen 👋"    
+}
+
+greetUser("Howdy")
+
+const welcomeEl = document.getElementById("welcome-el")
+
+// Add the ability to choose the emoji as well!
+
+function greetUser(greeting, name, emoji) {
+    welcomeEl.textContent = `${greeting}, ${name} ${emoji}`
+}
+
+greetUser("Howdy", "James", "👋")
+
+
+// Create a function, add(), that adds two numbers together and returns the sum
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+console.log( add(3, 4)   ) // should log 7
+console.log( add(9, 102) ) // should log 111
 */
 
 let myLeads = [];
