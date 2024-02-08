6b4e7d7b2da0350da444ac18d57d219fb34daa7b
giftedpromise <giftedpromise1992@gmail.com>
2024-02-08 04:07:29 +0100

    chrome extension

---
+++ mobileapp.js
@@ -0,0 +1,45 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL: "https://playground-e95c4-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const moviesInDB = ref(database, "movies");
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
+
+  push(moviesInDB, inputValue);
+
+  console.log(`${inputValue} added to database`);
+});
+
+/* Turning an object into an array 
+
+let scrimbaUsers = {
+    "00": "sindre@scrimba.com",
+    "01": "per@scrimba.com",
+    "02": "frode@scrimba.com"
+}
+
+// Challenge: Create a let variable called 'scrimbaUsersEmails' and use one of Object methods to set it equal to an array with the values
+let scrimbaUsersEmails = Object.values(scrimbaUsers)
+console.log(scrimbaUsersEmails)
+// Challenge: Create a let variable called 'scrimbaUsersIDs' and use one of Object methods to set it equal to an array with the keys
+let scrimbaUsersIDs = Object.keys(scrimbaUsers)
+console.log(scrimbaUsersIDs)
+// Challenge: Create a let variable called 'scrimbaUsersEntries' and use one of Object methods to set it equal to an array with the both the keys and values
+let scrimbaUsersEntries = Object.keys(scrimbaUsers)
+console.log(scrimbaUsersEntries)
+
+*/
